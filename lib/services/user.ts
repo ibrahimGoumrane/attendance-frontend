566e1a3a55e2610dc6254b6e37166846afbafe69
Arch <wbhaoui@gmail.com>
2025-03-28 22:40:31 +0000

    Add getLoggedInUser function

--- lib/services/user.ts
+++ lib/services/user.ts
@@ -1,38 +1,7 @@
-import { cookies } from "next/headers";
-import { sessionSchema, userSchema } from "../schemas/user";
-import { serverFetch } from "../serverUtils"
+import { serverFetch } from "../serverUtils";
 
-
-export async function getUserFromSession() {
-  const cookieStore = await cookies();
-  const sessionId = cookieStore.get("connect.sid");
-  if (!sessionId) return null;
-
-  const res = await serverFetch(`${process.env.API_URL}/session`);
-  console.log(res);
-  if (!res.ok) return null;
-  const rawSession = await res.json();
-  const { success, data : user } = sessionSchema.safeParse(rawSession);
-  if (!success) {
-    return null;
-  }
-  return user;
-}
-
-export async function getUserFromDb() {
-  const cookieStore = await cookies();
-  const sessionId = cookieStore.get("connect.sid");
-  console.log(sessionId);
-  if (!sessionId) return null;
-
-  const res = await serverFetch(`${process.env.API_URL}/profile`);
-  console.log(res);
-  if (!res.ok) return null;
-  const rawUser = await res.json();
-  const { success, data : user } = userSchema.safeParse(rawUser);
-  console.log(success, user);
-  if (!success) {
-    return null;
-  }
-  return user;
+export async function getLoggedInUser() {
+  const user = await serverFetch(`${process.env.API_URL}/user`);
+  const userJson = await user.json();
+  return userJson;
 }
