072b1016e29ef47c5472939daf7ba778ea55bc0b
bal-wal <balhaoui.walid@ensam-casa.ma>
2025-03-29 17:04:51 +0000

    Factor out TeacherCard component from /admin/teachers

--- app/admin/teachers/page.tsx
+++ app/admin/teachers/page.tsx
@@ -1,13 +1,6 @@
 import { getAllTeachers } from "@/lib/services/teachers";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Badge } from "@/components/ui/badge";
-import { Mail, PlusCircle, Pencil, Trash } from "lucide-react";
+import TeacherCard from "@/app/components/admin/TeacherCard";
+import { PlusCircle } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
 export default async function Teachers() {
@@ -22,51 +15,7 @@ export default async function Teachers() {
       </h1>
       <div className="mt-4 grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))]">
         {teachers.map((teacher) => (
-          <Card className="gap-2 py-4" key={teacher.id}>
-            <CardHeader className="pb-2 flex items-center gap-2">
-              <CardTitle className="text-xl ml-2">
-                {teacher.user.firstName} {teacher.user.lastName}
-              </CardTitle>
-              <Badge variant="outline" className="w-fit text-[0.75rem]">
-                {teacher.department}
-              </Badge>
-            </CardHeader>
-            <CardContent>
-              <div className="flex items-center text-sm text-slate-700">
-                <a href={`mailto:${teacher.user.email}`}>
-                  <Button
-                    className="cursor-pointer mr-2 border-2 rounded-full p-0"
-                    variant={"ghost"}
-                    size={"icon"}
-                  >
-                    <Mail className="size-5" />
-                  </Button>
-                </a>
-                <a
-                  className="hover:text-slate-900 hover:underline"
-                  href={`mailto:${teacher.user.email}`}
-                >
-                  {teacher.user.email}
-                </a>
-              </div>
-            </CardContent>
-            <CardFooter className="flex justify-end">
-              <Button
-                className="cursor-pointer mr-2 border-1 rounded-sm size-8 p-0"
-                variant={"ghost"}
-                size={"icon"}
-              >
-                <Pencil className="size-4" />
-              </Button>
-              <Button
-                className="border-red-500 text-red-500 bg-white hover:bg-red-500 hover:text-white size-8 p-0"
-                variant="outline"
-                size="icon"
-              >
-                <Trash className="size-4" />
-              </Button>
-            </CardFooter>
-          </Card>
+          <TeacherCard key={teacher.id} teacher={teacher} />
         ))}
       </div>
     </>
---
+++ app/components/admin/TeacherCard.tsx
@@ -0,0 +1,61 @@
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+import { Mail, Pencil, Trash } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Teacher } from "@/lib/types/api";
+
+export default function TeacherCard({teacher} : {teacher: Teacher}) {
+  return (
+    <Card className="gap-2 py-4" key={teacher.id}>
+    <CardHeader className="pb-2 flex items-center gap-2">
+      <CardTitle className="text-xl ml-2">
+        {teacher.user.firstName} {teacher.user.lastName}
+      </CardTitle>
+      <Badge variant="outline" className="w-fit text-[0.75rem]">
+        {teacher.department}
+      </Badge>
+    </CardHeader>
+    <CardContent>
+      <div className="flex items-center text-sm text-slate-700">
+        <a href={`mailto:${teacher.user.email}`}>
+          <Button
+            className="cursor-pointer mr-2 border-2 rounded-full p-0"
+            variant={"ghost"}
+            size={"icon"}
+          >
+            <Mail className="size-5" />
+          </Button>
+        </a>
+        <a
+          className="hover:text-slate-900 hover:underline"
+          href={`mailto:${teacher.user.email}`}
+        >
+          {teacher.user.email}
+        </a>
+      </div>
+    </CardContent>
+    <CardFooter className="flex justify-end">
+      <Button
+        className="cursor-pointer mr-2 border-1 rounded-sm size-8 p-0"
+        variant={"ghost"}
+        size={"icon"}
+      >
+        <Pencil className="size-4" />
+      </Button>
+      <Button
+        className="border-red-500 text-red-500 bg-white hover:bg-red-500 hover:text-white size-8 p-0"
+        variant="outline"
+        size="icon"
+      >
+        <Trash className="size-4" />
+      </Button>
+    </CardFooter>
+  </Card>
+  );
+}
