3725bb26225814ed03a28b420db23464705ad17a
bal-wal <balhaoui.walid@ensam-casa.ma>
2025-03-29 22:55:28 +0000

    Add shadcn Dialog wrapper

---
+++ app/components/Dialog.tsx
@@ -0,0 +1,38 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+import { AppDialogProps } from "@/lib/types/dialog";
+
+export default function AppDialog({ 
+  trigger, 
+  title, 
+  description, 
+  content, 
+  footer, 
+  header 
+}: AppDialogProps) {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>{trigger}</DialogTrigger>
+      <DialogContent>
+        {header}
+        {(title || description) && (
+          <DialogHeader>
+            {title && <DialogTitle>{title}</DialogTitle>}
+            {description && <DialogDescription>{description}</DialogDescription>}
+          </DialogHeader>
+        )}
+        {content}
+        {footer && <DialogFooter>{footer}</DialogFooter>}
+      </DialogContent>
+    </Dialog>
+  );
+}
+
---
+++ lib/types/dialog.ts
@@ -0,0 +1,8 @@
+export interface AppDialogProps {
+  trigger: React.ReactNode;
+  title?: React.ReactNode;
+  description?: React.ReactNode;
+  content?: React.ReactNode;
+  footer?: React.ReactNode;
+  header?: React.ReactNode;
+}
