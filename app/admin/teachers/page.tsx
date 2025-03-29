038e6468cf0d86a1d3912b33376a9ad909171d2b
bal-wal <balhaoui.walid@ensam-casa.ma>
2025-03-29 17:00:26 +0000

    Add edit and delete buttons

--- app/admin/teachers/page.tsx
+++ app/admin/teachers/page.tsx
@@ -1,18 +1,28 @@
 import { getAllTeachers } from "@/lib/services/teachers";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
-import { Mail, PlusCircle } from "lucide-react";
+import { Mail, PlusCircle, Pencil, Trash } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
 export default async function Teachers() {
   const teachers = await getAllTeachers();
-
   return (
     <>
-      <h1 className="font-bold text-2xl flex items-center gap-2">Teachers <Button size={"sm"} className="ml-auto text-xs" variant={"outline"}><PlusCircle/> Add New</Button></h1>
+      <h1 className="font-bold text-2xl flex items-center gap-2">
+        Teachers{" "}
+        <Button size={"sm"} className="ml-auto text-xs" variant={"outline"}>
+          <PlusCircle /> Add New
+        </Button>
+      </h1>
       <div className="mt-4 grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))]">
         {teachers.map((teacher) => (
-          <Card className="gap-2" key={teacher.id}>
+          <Card className="gap-2 py-4" key={teacher.id}>
             <CardHeader className="pb-2 flex items-center gap-2">
               <CardTitle className="text-xl ml-2">
                 {teacher.user.firstName} {teacher.user.lastName}
@@ -40,6 +50,22 @@ export default async function Teachers() {
                 </a>
               </div>
             </CardContent>
+            <CardFooter className="flex justify-end">
+              <Button
+                className="cursor-pointer mr-2 border-1 rounded-sm size-8 p-0"
+                variant={"ghost"}
+                size={"icon"}
+              >
+                <Pencil className="size-4" />
+              </Button>
+              <Button
+                className="border-red-500 text-red-500 bg-white hover:bg-red-500 hover:text-white size-8 p-0"
+                variant="outline"
+                size="icon"
+              >
+                <Trash className="size-4" />
+              </Button>
+            </CardFooter>
           </Card>
         ))}
       </div>
