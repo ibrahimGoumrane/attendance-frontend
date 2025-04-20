99581475ea85ca4d310b77331e3be059fe0379aa
Arch <wbhaoui@gmail.com>
2025-04-20 17:16:39 +0100

    Add student placeholder image

--- components/admin/display/StudentCard.tsx
+++ components/admin/display/StudentCard.tsx
@@ -1,11 +1,16 @@
 import { Student } from "@/lib/types/api";
 import { DeleteStudentDialog } from "../dialogs/StudentDialogs";
+import { Card, CardTitle } from "@/components/ui/card";
+import Image from "next/image";
 
 export default function StudentCard({ student }: { student: Student }) {
   return (
-    <div className="border border-red-300">
-      {student.user.firstName + " " + student.user.lastName}
-      <DeleteStudentDialog student={student} />
-    </div>
+    <Card className="flex flex-col items-center">
+      <CardTitle className="text-xl flex items-center gap-2">
+        {student.user.firstName + " " + student.user.lastName}
+        <DeleteStudentDialog student={student} />
+      </CardTitle>
+      <Image alt="Student image" width={225} height={225} src={"/student_placeholder.jpg"}></Image>
+    </Card>
   );
 }
